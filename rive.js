f09a18f2793abf72fd3a90c4680094dbab7e7932
MBP27 <MBP27@mac.home>
2025-08-13 17:42:37 +0200

    base rive

---
+++ rive.js
@@ -0,0 +1,26 @@
+const stateMachine = "State Machine 1";
+
+const riveInstance = new rive.Rive({
+	src: "./the_arcane-chemin.riv", //get rive file
+	canvas: document.getElementById("rive"), //get correct canvas
+	autoplay: true,
+	stateMachines: stateMachine, // get correct stateMachine
+	automaticallyHandleEvents: true, // Automatically handle RiveHTTPEvents
+	onLoad: onLoadHandler,
+});
+
+// Resize the drawing surface if the window resizes
+window.addEventListener(
+	"resize",
+	() => {
+		// riveInstance.resizeDrawingSurfaceToCanvas();
+	},
+	false
+);
+
+// Handle the onLoad event
+function onLoadHandler() {
+	// Prevent a blurry canvas by using the device pixel ratio
+	riveInstance.resizeDrawingSurfaceToCanvas();
+	inputs = riveInstance.stateMachineInputs(stateMachine);
+}
